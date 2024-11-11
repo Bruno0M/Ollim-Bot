6d4907d31e87d7b1a624af769b15091f971da228
Bruno0M <brunomedeirosr09@gmail.com>
2024-11-10 21:07:44 -0300

    fix: adjust the DI

--- Ollim.Bot/Commands/Modules/TestGeminiCommandHandler.cs
+++ Ollim.Bot/Commands/Modules/TestGeminiCommandHandler.cs
@@ -1,29 +1,34 @@
 ﻿using Discord;
 using Discord.Interactions;
-using Newtonsoft.Json;
 using Ollim.Bot.Services;
 
 namespace Ollim.Bot.Commands.Modules
 {
     public class TestGeminiCommandHandler : InteractionModuleBase<SocketInteractionContext>
     {
+        private readonly GeminiService _geminiService;
+
+        public TestGeminiCommandHandler(GeminiService geminiService)
+        {
+            _geminiService = geminiService;
+        }
+
         [SlashCommand("gemininho", "testano essa tal de AI")]
         public async Task Gemininho(string prompt, Attachment attachment = null)
         {
             await DeferAsync();
 
-            var gemi = new GeminiService();
             var generateContent = "";
 
             if (attachment != null)
             {
                 var filePath = attachment.Filename;
-                var response = await gemi.UploadFile(attachment.Url, filePath, "application/pdf");
-                generateContent = await gemi.GenerateContent(prompt, response, filePath);
+                var response = await _geminiService.UploadFile(attachment.Url, filePath, "application/pdf");
+                generateContent = await _geminiService.GenerateContent(prompt, response, filePath);
             }
             //var prompt = "Summarize what's in this pdf";
 
-            generateContent = await gemi.GenerateContent(prompt);
+            generateContent = await _geminiService.GenerateContent(prompt);
 
             await ModifyOriginalResponseAsync(props => props.Content = generateContent);
             //await ModifyOriginalResponseAsync(props => props.Content = "Ok");
